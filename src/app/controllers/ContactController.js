1afa19a01205d2bd58029454e1408e8a4ce073fd
Yan Garcia <yangarciavaz@gmail.com>
2025-03-25 09:41:20 -0300

    Creating the Contacts creation route

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -21,8 +21,25 @@ class ContactController {
     return response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // Criar um novo registro (Create)
+    const {name, email, phone, category_id } = request.body;
+
+    if(!name) {
+      return response.status(400).json({error: 'Name is required.'});
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if(contactExists) {
+      return response.status(400).json({error: 'Email already taken'});
+    }
+
+    const contact = await ContactsRepository.create({
+      name, email, phone, category_id
+    });
+
+    response.json(contact);
   }
 
   update() {
--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,10 @@ const ContactController = require('./app/controllers/ContactController');
 
 const router = Router();
 
-router.get(
-  '/contacts',
-  (request, response, next) => {
-    request.appId = "MeuAppIDAqui";
-    next();
-  },
-  ContactController.index
-);
+router.get('/contacts',ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
+router.post('/contacts/', ContactController.store);
 
 
 
