26900439f95f12cac2c431126e42a518ba2cb0c9
Yan Garcia <yangarciavaz@gmail.com>
2025-04-01 10:50:05 -0300

    feat: refactoring Delete method

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -70,14 +70,13 @@ class ContactController {
   }
 
   async delete(request, response) {
-    // Deletar um registro
     const { id } = request.params;
 
-    const contact = await ContactsRepository.findById(id);
+    // const contact = await ContactsRepository.findById(id);
 
-    if (!contact) {
-      return response.status(404).json({ error: 'User not found' });
-    }
+    // if (!contact) {
+    //   return response.status(404).json({ error: 'User not found' });
+    // }
 
     await ContactsRepository.delete(id);
     // 204: Valid request but No Content
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid')
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Yan',
-    email: 'yan@gmail.com',
-    phone: '319111111',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Fulano',
-    email: 'fulano@gmail.com',
-    phone: '819111111',
-    category_id: v4(),
-  },
-]
 
 class ContactsRepository {
 
@@ -65,11 +46,13 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query(`
+      DELETE
+      FROM contacts
+      WHERE id = $1
+      `, [id]);
+    return deleteOp;
   }
 
 
